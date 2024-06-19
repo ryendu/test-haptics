3d713d83c4a6a97459829382beeae55ab1c00cca
Ryan Du <ryandu9221@gmail.com>
2024-06-19 01:22:03 -0400

    Initial Commit

---
+++ test-haptics/test_hapticsApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_hapticsApp.swift
+//  test-haptics
+//
+//  Created by Ryan Du on 6/19/24.
+//
+
+import SwiftUI
+
+@main
+struct test_hapticsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
